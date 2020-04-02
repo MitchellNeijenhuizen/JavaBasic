898a7a9d0859c23df4fb2bdb5874ecfa1da3d6ea
Mitchell <m.a.j.w.neijenhuizen>
2020-04-02 14:59:46 +0200

    This program demonstrates the functionality of an if/else statements. Version #1

--- src/com/company/DemonstrationOfIfBlock.java
+++ src/com/company/DemonstrationOfIfBlock.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class DemonstrationOfIfBlock {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter a value for x: ");
         double x = scanner.nextDouble();
